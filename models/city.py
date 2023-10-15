a3f606973a270d9d49b5c541d3fb3ab99e5e1155
Abadamasi <abbabadamasi33@gmail.com>
2023-10-15 22:46:38 +0000

    console

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines the City class."""
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """Represent an city.
+
+    Attributes:
+        state_id (str): The state id.
+    """
+
+    state_id = ""
+    name = ""
---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a User class"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """Represents a User by various attributes"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    second_name = ""
